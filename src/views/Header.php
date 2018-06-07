dab9d65e481b47b568f94c0caedbff59290d8ff9
Ivan <madritistaaa@gmail.com>
2018-06-07 13:48:16 +0300

    Use isset

--- src/views/Header.php
+++ src/views/Header.php
@@ -3,8 +3,9 @@
           <h3 class="masthead-brand">Group Concert</h3>
           <nav class="nav nav-masthead justify-content-center">
           <?php
-            if($_SESSION['current_user_id']){
-            } else{
+            if(isset($_SESSION['current_user_id'])){
+              echo '<a class="nav-link" href="../controllers/Logout.php">Logout</a>';
+             } else{
               echo '<a class="nav-link" href="./Register.php">Register</a>';
               echo '<a class="nav-link" href="./Login.php">Login</a>';
             }
