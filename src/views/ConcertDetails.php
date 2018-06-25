d438af34f5d4c8406c235303b88163c26fbf0553
Ivan <madritistaaa@gmail.com>
2018-06-25 10:09:06 +0300

    Add view

---
+++ src/views/ConcertDetails.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Group Concert</title>
+
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="../assets/css/main.css" rel="stylesheet">
+
+  </head>
+
+  <body class="text-center">
+
+    <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <?php session_start();
+include './Header.php'?>
+
+      <main role="main" class="inner cover">
+        <header>
+                                <h1><?=$concert->getTitle()?></h1>
+                                <?php 
+                                    if($concert->getIsActive())
+                                    {
+                                        echo '<p>'.$concert->getDate().'</p>';
+                                    }
+                                    else
+                                    {
+                                        echo '<p>This concert has finished.</p>';
+                                    }
+                                ?>
+                            </header>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+        <p class="copyright">&copy; WWW 10th ed, Group Concert by I.Mladenov (61950). All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
