5dbf2a80f0d16a7c6c00e2e88c190e10b64482a1
Ivan <madritistaaa@gmail.com>
2018-06-25 15:16:10 +0300

    Implement edit concert

---
+++ src/controllers/GetEditConcert.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../libs/Startup.php";
+Startup::_init(true);
+use models\Concert;
+use models\User;
+use models\Error;
+
+session_start();
+
+if(!isset($_SESSION['current_user_id'])) {
+    http_response_code(401);
+ 	// header('Location: ../views/Error.php?message=Only authenticated users can view project details.&status_code=401');
+} else {
+	$current_user = $_SESSION['current_user_id'];
+	
+	$concert_id = $_GET['id'];
+	$concert = Concert::getById($concert_id);
+
+  if($concert->getHostId() === $current_user){
+	if($concert->getTitle()) {
+	  require_once('../views/EditConcert.php');	
+	} else {
+		http_response_code(404);
+ 		// header('Location: ../views/Error.php?message=Project was not found.&status_code=404');
+	}
+  } else {
+    http_response_code(401);
+  }
+
+}
+?>
---
+++ src/views/EditConcert.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Group Concert</title>
+
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="../assets/css/main.css" rel="stylesheet">
+
+  </head>
+
+  <body class="text-center">
+
+    <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <?php session_start();
+include './Header.php'?>
+
+      <main role="main" class="inner cover">
+      <form class="custom-form" method="post" action="../controllers/EditConcert.php">
+
+      <input type="text" id="title" name="title" class="form-control" placeholder="Title" required value="<?=$concert->getTitle()?>">
+      <div class="error">
+                <?php if (isset($_GET['title']) && $_GET['title'] == 'false'): ?>
+                    Title is required
+                                            <?php endif?>
+                                        </div>
+
+                                         <input type="text" id="city" name="city" class="form-control" placeholder="City" required value="<?=$concert->getCity()?>"  >
+      <div class="error">
+                                            <?php if (isset($_GET['city']) && $_GET['city'] == 'false'): ?>
+                                                City is required
+                                            <?php endif?>
+                                        </div>
+
+                                         <input type="text" id="address" name="address" class="form-control" placeholder="Address" required value="<?=$concert->getAddress()?>" >
+      <div class="error">
+                                            <?php if (isset($_GET['address']) && $_GET['address'] == 'false'): ?>
+                                                Address is required
+                                            <?php endif?>
+                                        </div>
+
+<input type="date" id="date" name="date" class="form-control" placeholder="Date" required value="<?=$concert->getDate()()?>" >
+      <div class="error">
+                                            <?php if (isset($_GET['date']) && $_GET['date'] == 'false'): ?>
+                                                Date is required
+                                            <?php endif?>
+                                        </div>
+
+                                         <input type="number" id="spots" name="spots" class="form-control" placeholder="Spots" value="<?=$concert->getSpots()?>"  >
+
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Update</button>
+    </form>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+        <p class="copyright">&copy; WWW 10th ed, Group Concert by I.Mladenov (61950). All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
