d49fe9c9385e7b4a4747a0c9ea7900c8689ea87f
Ivan <madritistaaa@gmail.com>
2018-06-30 11:19:25 +0300

    Navigate to list after login and logout

--- src/controllers/Login.php
+++ src/controllers/Login.php
@@ -20,7 +20,7 @@ if (!$is_username_valid || !$is_password_valid) {
     	$_SESSION['current_user_username'] = $user->getUsername();
     	$_SESSION['current_user_id'] = $user->getId();
 
-    	header('Location: ../views/GetAllConcerts.php');
+    	header('Location: ../controllers/GetAllConcerts.php');
     }
     else
     {
--- src/controllers/Logout.php
+++ src/controllers/Logout.php
@@ -3,6 +3,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location:../views/Home.php");
+header("Location: ../controllers/GetAllConcerts.php");
 exit();
 ?>
